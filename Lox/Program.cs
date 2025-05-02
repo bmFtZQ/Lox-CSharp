5aac4f179ccda19e60c85d117d81ec018700dcad
bmFtZQ <62812711+bmFtZQ@users.noreply.github.com>
2025-05-02 20:56:47 +0800

    Add basic program functionality

--- Lox/Program.cs
+++ Lox/Program.cs
@@ -2,8 +2,59 @@
 
 internal static class Program
 {
+    internal static bool HadError = false;
+
     internal static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        switch (args.Length)
+        {
+            case > 1:
+                Console.WriteLine("Usage: Lox <file>");
+                Environment.Exit(64);
+                break;
+
+            case 1:
+                RunFile(args[0]);
+                break;
+
+            default:
+                RunPrompt();
+                break;
+        }
+    }
+
+    internal static void RunFile(string filename)
+    {
+        var source = File.ReadAllText(filename);
+        Run(source);
+
+        if (HadError) Environment.Exit(65);
+    }
+
+    internal static void RunPrompt()
+    {
+        while (true)
+        {
+            Console.Write("> ");
+            var line = Console.ReadLine();
+            if (line is null) break;
+            Run(line);
+            HadError = false;
+        }
+    }
+
+    internal static void Run(string source)
+    {
+    }
+
+    internal static void Error(int line, string message)
+    {
+        Report(line, "", message);
+    }
+
+    internal static void Report(int line, string where, string message)
+    {
+        Console.Error.WriteLine($"[line {line}] Error{where}: {message}");
+        HadError = true;
     }
 }
